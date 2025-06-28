358a3fd64858bfda2eb3ac6b2b5bbe362cd1cd69
Shivam Gupta <kapilmohangupta20@gmail.com>
2025-06-28 22:57:29 +0530

    bug fixed in index!

--- backend/src/index.js
+++ backend/src/index.js
@@ -37,8 +37,8 @@ app.use("/api/otp", otpRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
